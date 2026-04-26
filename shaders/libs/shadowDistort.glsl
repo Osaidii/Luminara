a1919ba871130a51d11b5c24629dd0f75ac6d1a1
Osaid Hassan <hassanosaid123@gmail.com>
2026-04-26 13:34:21 +0200

    removed shadows, do shadow from scratch

--- shaders/libs/shadowDistort.glsl
+++ shaders/libs/shadowDistort.glsl
@@ -1,6 +1,6 @@
 const bool shadowtex0Nearest = true;
 const bool shadowtex1Nearest = true;
-const bool shadowcolor0Neastest = true;
+const bool shadowcolor0Nearest = true;
 
 vec3 distortShadowClipPos(vec3 shadowClipPos) {
     float distortionFactor = length(shadowClipPos.xy);
